ae39030ab020599afbc5c0f5852dd3541743f1ad
Rafael Abras <rafaelabraslessa@gmail.com>
2025-05-29 22:25:40 -0300

    feat: conexão via WebSocket com SignalR configurada com organizações por sessões, envio de mensagens, e persistencia no DB.

--- Hubs/ChatHubs.cs
+++ Hubs/ChatHubs.cs
@@ -1,18 +1,47 @@
 ﻿using DevMatch.Interfaces;
+using DevMatch.Models;
+using Microsoft.AspNetCore.Connections;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
 
 namespace DevMatch.Hubs
 {
     public sealed class ChatHubs : Hub
     {
+        private readonly IMessageRepository _messageRepository;
+        public ChatHubs(IMessageRepository messageRepository)
+        {
+            _messageRepository = messageRepository;
+        }
         public override async Task OnConnectedAsync()
         {
-            await Clients.All.SendAsync( $"{Context.ConnectionId} has joined");
+            var user = Context.UserIdentifier;
+            var sessionId = Context.GetHttpContext()?.Request.Query["sessionId"];
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, $"user-{user}");
+
+            if (!string.IsNullOrEmpty(sessionId))
+                await Groups.AddToGroupAsync(Context.ConnectionId, $"chat-{sessionId}");
+
+            await Clients.Caller.SendAsync("ReceiveMessage", $"Usuario: {user} conectado!");
+
         }
 
-        public async Task SendMessage(string message)
+        public async Task SendMessageToSession(string message, int sessionId)
         {
-            await Clients.All.SendAsync($"{Context.ConnectionId}: {message}");
+            var senderId = Context.UserIdentifier;
+
+            var chatMessage = new ChatMessage
+            {
+                SessionId = sessionId,
+                SenderId = senderId,
+                Conteudo = message
+            };
+
+            await Clients.Group($"chat-{sessionId}").
+                SendAsync("ReceiveMessage", chatMessage);
+
+            await _messageRepository.SaveMessageAsync(chatMessage);
         }
 
     }
