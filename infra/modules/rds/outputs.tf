1c86e7696d3d3514c8449d130eed1b5ca92090bb
Rafael Abras <rafaelabraslessa@gmail.com>
2025-08-16 12:45:17 -0300

    feat: dockerfile para o ecr porem ainda hardcoded antes do CD

--- infra/modules/rds/outputs.tf
+++ infra/modules/rds/outputs.tf
@@ -2,3 +2,19 @@ output "rds_id" {
   description = "ID da instancia do RDS"
   value       = aws_db_instance.db_sqlserver.id
 }
+
+output "rds_endpoint" {
+  value = aws_db_instance.db_sqlserver.endpoint
+}
+
+output "rds_port" {
+  value = aws_db_instance.db_sqlserver.port
+}
+
+output "rds_username" {
+  value = aws_db_instance.db_sqlserver.username
+}
+
+output "rds_db_name" {
+  value = aws_db_instance.db_sqlserver.db_name
+}
